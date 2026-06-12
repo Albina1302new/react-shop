a86c190ec2479c427513307bdb6ab299d6ab0cfa
albina_biriukova <Alja.birjukova@gmail.com>
2026-06-12 19:01:46 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/react-shop">
+    
       <App />
-    </BrowserRouter>
+   
   </StrictMode>,
 );
